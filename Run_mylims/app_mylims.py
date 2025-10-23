e9895534bfbbfe2b5076fdde3de8e351c39a112f
kasmiyassin <yassine.kasmi@thuenen.de>
2025-10-23 16:05:11 +0200

    update user name

--- Run_mylims/app_mylims.py
+++ Run_mylims/app_mylims.py
@@ -16,7 +16,7 @@ load_dotenv()
 # --- Configuration ---
 DB_HOST: str = os.getenv('DB_HOST', '0.0.0.0')
 DB_NAME: str = os.getenv('DB_NAME', 'mylims')
-DB_USER: str = os.getenv('DB_USER', 'kasmi')
+DB_USER: str = os.getenv('DB_USER', 'web_admin')
 DB_PASS: str = os.getenv('DB_PASS', 'password')
 SECRET_KEY: str = os.getenv('SECRET_KEY', 'a_very_secret_key_for_session_management_and_security')
 
